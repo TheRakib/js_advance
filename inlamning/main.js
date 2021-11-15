dc54d467c10cd5a53dd0e84f47f5c8cc993e1ea2
RakibTest <rakibmedieinstitutet@gmail.com>
2021-11-15 13:50:41 +0100

    inlamning 2

--- inlamning/main.js
+++ inlamning/main.js
@@ -5,35 +5,63 @@
 
 document.querySelector("button").addEventListener("click", calculateBudget)
 
+  
+let kostnadLista = [];
+let inkomstLista = [];
 function calculateBudget(e) {
     e.preventDefault();
   
    //console.log("button works")
-    
+// pusha kostnader i kostnadLista 
+// pusha inkomster  i inkomstLista 
+
+//Loopa igenom listorna och lagra totalt inkomst och kostnad
+
+// vinst = inkomst -kostnad 
+//  visa vinst i en  div 
 
 const option= document.querySelector("select");
 console.log(option.value);
 // om användare väljer + ska description och value hamnas i inkomst-lista diven
 if(option.value=="+"){    
     //console.log(addOption)
+
   const description =  document.querySelector("#desc").value;
   const value = document.querySelector("#num").value;
+
+  inkomstLista.push(value)
  const div= document.querySelector(".inkomst-lista");
  // console.log(div)
  div.innerHTML   += `<li>${description}     ${value}</li>`
-
-
 }
  //om användare väljer - ska description och value hamnas i kostnad-lista diven
-
 if(option.value =="-"){
     //console.log(minusOption)
     const description = document.querySelector("#desc").value;
     const value = document.querySelector("#num").value;
+    kostnadLista.push(value)
     const kostnadDiv= document.querySelector(".kostnad-lista");
    // console.log(kostnadDiv)
     kostnadDiv.innerHTML += `<li>${description}     ${value}</li>`;
  }
+
+ console.log(inkomstLista , kostnadLista)
+
+
+var kostnadSumma= 0;
+ for( var i= 0; i<kostnadLista.length ; i++){
+     kostnadSumma += Number(kostnadLista[i]);
+ }
+ var inkomstSumma=0;
+ for( var i= 0; i<inkomstLista.length; i++)
+ { 
+     inkomstSumma += Number(inkomstLista[i])
+ }
+ console.log(inkomstSumma, kostnadSumma)
+
+
+ const vinstDiv = document.querySelector(".vinst")
+ vinstDiv.textContent = inkomstSumma-kostnadSumma
 //const chooseOption = document.querySelector(".choose").textContent;
 //console.log(chooseOption);
 //if()
@@ -42,7 +70,6 @@ if(option.value=="choose")
     alert("Hej, Du måste välja från  + och -")
 }
   
-
 }
 
 
