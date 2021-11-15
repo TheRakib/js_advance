fd03441c4e8ea7699e5ba5d137f2516e32314306
RakibTest <rakibmedieinstitutet@gmail.com>
2021-11-15 11:44:41 +0100

    inlamning 2

--- inlamning/main.js
+++ inlamning/main.js
@@ -1,2 +1,25 @@
 
-// 
+// när anavädare trycker på add button :- 
+
+// event :- addEventListener()
+
+document.querySelector("button").addEventListener("click", calculateBudget)
+
+
+
+function calculateBudget(e) {
+    e.preventDefault();
+  
+   console.log("button works")
+    // om användare väljer + ska description och value hamnas i inkomst-lista diven
+
+const addOption= document.querySelector(".plus").textContent;
+console.log(addOption)
+const minusOption =document.querySelector(".minus").textContent;
+console.log(minusOption);
+const chooseOption = document.querySelector(".choose").textContent;
+console.log(chooseOption);
+//if()
+   //om användare väljer - ska description och value hamnas i kostnad-lista diven
+
+}
