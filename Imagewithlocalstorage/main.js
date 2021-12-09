7ba3f035ac223a63afaf04ee4b362f9b768b1775
RakibTest <rakibmedieinstitutet@gmail.com>
2021-12-09 09:30:32 +0100

    admin.html

---
+++ Imagewithlocalstorage/main.js
@@ -0,0 +1,20 @@
+function addProduct(e) {
+
+    e.preventDefault();
+
+
+// vi läser in input fältet och sparar data in i localstorage
+
+const src= document.querySelector("#imgSrc").value;
+
+const productName= document.querySelector("#productName").value
+
+localStorage.setItem("src", src)
+
+localStorage.setItem("productName", productName)
+
+
+
+}
+
+document.querySelector("button").addEventListener("click" , addProduct);
